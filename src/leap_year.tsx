0356a8885961693190a59e1a12cf9f93f3d11253
tedwards <tedwards@assessor.com.au>
2022-08-15 11:49:52 +1000

    Basic screen

--- src/leap_year.tsx
+++ src/leap_year.tsx
@@ -3,21 +3,27 @@ import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
-import Button from '@mui/material/Button'
-import TextField from '@mui/material/TextField'
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import { Divider, InputLabel } from '@mui/material';
+import AdUnitsIcon from '@mui/icons-material/AdUnits';
 import './App.css';
 require('react-dom');
 
 function LeapYearCalc() {
   return (    
   <>
-    <h1>Hello</h1>
-    <Button variant="contained">Hello World</Button><br/>
-    <label htmlFor="endYear">End Year:</label>
+    <h1>Leap Year Calculator</h1>
+
     <TextField
       id="endYear"
-      placeholder="Enter end year of calculation"
+      label="Enter end year of calculation"
+      variant="standard"
       type="number" />
+      <Divider />
+      <Button variant="contained" startIcon={<AdUnitsIcon />} size="large" onClick={() => {
+        alert('Calculated');
+      }}>Calculate</Button>
   </>
   );
 }
